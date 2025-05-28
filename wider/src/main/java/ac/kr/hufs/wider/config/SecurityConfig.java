faa3c48f3db7afc6c317f65a4b11d807383de80d
KwonYeonghoo <kupa10000@naver.com>
2025-05-28 14:45:44 +0900

    CORS 정책 설정 3트

--- wider/src/main/java/ac/kr/hufs/wider/config/SecurityConfig.java
+++ wider/src/main/java/ac/kr/hufs/wider/config/SecurityConfig.java
@@ -39,7 +39,8 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(ALLOWED_ORIGINS);
+        // configuration.setAllowedOrigins(ALLOWED_ORIGINS);
+        configuration.setAllowedOrigins(Arrays.asList("*"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "X-Requested-With"));
         configuration.setExposedHeaders(Arrays.asList("Authorization"));
